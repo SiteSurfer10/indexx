ed6e06b81b4e7403b4793ac6c89f62f6b8c4113f
SiteSurfer10 <114467127+SiteSurfer10@users.noreply.github.com>
2023-12-03 12:13:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,20 @@
 </head>
  
 <body>
+
+ <p>probeersel</p>
+
+ <?php 
+ Echo "<html>";
+Echo
+"<title>HTML With PHP</title>";
+Echo
+"<b>My Example</b>";
+//your php code here
+Print
+"<i>Print works too!</i>"; 
+?>
+
  
 <h1><?php echo "This message is from server side." ?></h1>
  
